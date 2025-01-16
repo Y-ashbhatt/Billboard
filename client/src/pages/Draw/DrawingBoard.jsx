4fd6291bd3bad1a597568f3bfd9ab75f8405b3a0
Yash Bhatt <30y.bhatt@gmail.com>
2025-01-16 20:46:25 +0530

    UI : change

--- client/src/pages/Draw/DrawingBoard.jsx
+++ client/src/pages/Draw/DrawingBoard.jsx
@@ -4,25 +4,25 @@ import { Excalidraw } from "@excalidraw/excalidraw";
 
 const DrawingBoard = () => {
   return (
-    <>
-      <div className="bg-gray-100 min-h-screen flex">
-        {/* Sidebar */}
+    <div className="bg-gray-100 min-h-screen flex">
+      {/* Sidebar */}
+      <div className="w-64">
         <Sidebar />
-
-        {/* Main Content */}
-        <main className="flex-1 flex flex-col">
-          <header className="p-6 bg-white shadow-md">
-            <h1 className="text-3xl font-bold text-gray-800 text-center">
-              Create Your Banner
-            </h1>
-          </header>
-
-          <div className="flex-1 bg-white shadow rounded-lg m-6">
-            <Excalidraw />
-          </div>
-        </main>
       </div>
-    </>
+
+      {/* Main Content */}
+      <main className="flex-1 flex flex-col">
+        <header className="p-4 bg-white shadow-md">
+          <h1 className="text-2xl font-bold text-gray-800 text-center">
+            Create Your Banner
+          </h1>
+        </header>
+
+        <div className="flex-1">
+          <Excalidraw style={{ width: "100%", height: "100%" }} />
+        </div>
+      </main>
+    </div>
   );
 };
 
